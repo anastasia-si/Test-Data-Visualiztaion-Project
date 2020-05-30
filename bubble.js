0999b9fbe375c1a0384950cd6b647fb5f5924a98
Anastasia <37953935+anastasia-si@users.noreply.github.com>
2020-05-30 11:26:46 -0400

    removed hard-coded values

--- bubble.js
+++ bubble.js
@@ -1,208 +1,232 @@
-function buildBubble(data)  {
+function generateBubble(settings) {
 
-// ---------------------------//
-//       AXIS  AND SCALE      //
-// ---------------------------//
+    const elemId = settings.element;
+    const tooltipSelector = settings.tooltip;
+    const margin = settings.margin;
+    const width = settings.size.width - margin.left - margin.right;
+    const height = settings.size.height - margin.top - margin.bottom;
 
-   // Add X axis
-    var mindate = new Date(1980,0,1),
-        maxdate = new Date(2020,0,31);
+    const languages = {"eng": "English", "en-CA": "English - Canada", "en-GB": "English - GB", "en-US": "English - US", "fre": "French", "ger": "German", "grc": "Greek", "spa": "Spanish"};
+    const lang_codes = Object.keys(languages);
 
-    var x = d3.scaleTime()
-        .domain([mindate, maxdate])
-    .range([0, width]);
+    var parseDate = d3.timeParse("%d.%m.%Y");
 
-    bubble_svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).ticks(5));
+    // Remove existing SVG element (to avoid duplicating when resizing)
+    clearSVG(elemId);
 
-    // X axis label:
-    bubble_svg.append("text")
-     .attr("text-anchor", "end")
-     .attr("x", width)
-     .attr("y", height + 30)
-     .attr("class", "bubble-axis-label")
-     .text("Publication Date");
+    // Tooltip div is hidden by default
+    var tooltip = d3.select(tooltipSelector)
 
-    // Y axis
-    var y = d3.scaleLinear()
-    .domain([2, 5])
-    .range([ height, 0]);
-    bubble_svg.append("g")
-    .call(d3.axisLeft(y));
+    var bubble_svg = d3.select(elemId)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+             "translate(" + margin.left + "," + margin.top + ")");
 
-    // Y axis label:
-    bubble_svg.append("text")
-     .attr("class", "bubble-axis-label")
-     .attr("x", 0)
-     .attr("y", -10)
-     .text("Rating")
+    d3.csv(settings.path, function(error, data) {
+        // ---------------------------//
+        //       AXIS  AND SCALE      //
+        // ---------------------------//
 
-    var ticks = d3.selectAll(".tick text");
-    ticks.attr("class", "bubble-axis-label");
+           // Add X axis
+            var x = d3.scaleTime()
+                .domain(d3.extent(data, function(d){ return parseDate(d.publication_date); }))
+                .range([0, width]);
 
-    // Scale for bubble size
-    var z = d3.scaleSqrt()
-    .domain([0, 7000])
-    .range([ 2, 30]);
+            bubble_svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x).ticks(5));
 
-    // Scale for bubble color
-    var bubbleColor = d3.scaleOrdinal()
-    .domain(lang_codes)
-    .range(d3.schemeSet1);
+            // X axis label:
+            bubble_svg.append("text")
+             .attr("text-anchor", "end")
+             .attr("x", width)
+             .attr("y", height + 30)
+             .attr("class", "bubble-axis-label")
+             .text("Publication Date");
 
+            // Y axis
+            var y = d3.scaleLinear()
+                .domain([2, 5]) // fixed rating - from 2 to 5
+                .range([height, 0]);
+                bubble_svg.append("g")
+                .call(d3.axisLeft(y));
 
-// ---------------------------//
-//      TOOLTIP               //
-// ---------------------------//
+            // Y axis label:
+            bubble_svg.append("text")
+             .attr("class", "bubble-axis-label")
+             .attr("x", 0)
+             .attr("y", -10)
+             .text("Rating")
 
-   var showTooltip = function(d) {
-        tooltip
-         .transition()
-         .duration(100)
-        tooltip
-         .style("opacity", 0.9)
-         .html("Book: <b>" + d.title + "</b><br>" +
-               "Author: <b>" + d.authors + "</b><br>" +
-               d.num_pages + " pages<br>" +
-               "Publication date: " + d.publication_date + "<br>" +
-               "Rating: " + d.average_rating + "<br>");
-    }
+            var ticks = d3.selectAll(".tick text");
+            ticks.attr("class", "bubble-axis-label");
 
-    var moveTooltip = function(d) {
-        tooltip
-          .style("top", d3.select(this).attr("cy") + "px")
-          .style("left", d3.select(this).attr("cx") + "px")
-    }
+            // Scale for bubble size
+            var z = d3.scaleSqrt()
+                .domain([0, d3.max(data.map(function(d) {return parseInt(d.num_pages);}))])
+                .range([2, 30]);
 
-    var hideTooltip = function(d) {
-    tooltip
-     .transition()
-     .duration(200)
-     .style("opacity", 0)
-    }
+            // Scale for bubble color
+            var bubbleColor = d3.scaleOrdinal()
+                .domain(lang_codes)
+                .range(d3.schemeSet1);
 
 
-// ---------------------------//
-//       HIGHLIGHT GROUP      //
-// ---------------------------//
+        // ---------------------------//
+        //      TOOLTIP               //
+        // ---------------------------//
 
-// When a legend category is hovered
-var highlight = function(d){
-    d3.selectAll(".bubbles").style("opacity", .05)
-    d3.selectAll("."+d).style("opacity", 1)
+           var showTooltip = function(d) {
+                tooltip
+                 .transition()
+                 .duration(100)
+                tooltip
+                 .style("opacity", 0.9)
+                 .html("Book: <b>" + d.title + "</b><br>" +
+                       "Author: <b>" + d.authors + "</b><br>" +
+                       d.num_pages + " pages<br>" +
+                       "Publication date: " + d.publication_date + "<br>" +
+                       "Rating: " + d.average_rating + "<br>");
+            }
+
+            var moveTooltip = function(d) {
+                tooltip
+                  .style("top", d3.select(this).attr("cy") + "px")
+                  .style("left", d3.select(this).attr("cx") + "px")
+            }
+
+            var hideTooltip = function(d) {
+            tooltip
+             .transition()
+             .duration(200)
+             .style("opacity", 0)
+            }
+
+
+        // ---------------------------//
+        //       HIGHLIGHT GROUP      //
+        // ---------------------------//
+
+        // When a legend category is hovered
+        var highlight = function(d){
+            d3.selectAll(".bubbles").style("opacity", .05)
+            d3.selectAll("."+d).style("opacity", 1)
+        }
+
+        // When a legend category is not hovered anymore
+        var noHighlight = function(d){
+            d3.selectAll(".bubbles").style("opacity", 1)
+        }
+
+
+        // ---------------------------//
+        //       CIRCLES              //
+        // ---------------------------//
+
+        bubble_svg.append('g')
+            .selectAll("dot")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("class", function(d) { return "bubbles " + d.language_code })
+            .attr("cx", function (d) {  return x(parseDate(d.publication_date)); })
+            .attr("cy", function (d) { return y(d.average_rating); })
+            .attr("r", function (d) { return z(d.num_pages); })
+            .style("fill", function (d) { return bubbleColor(d.language_code); })
+            .on("mouseover", showTooltip )
+            .on("mousemove", moveTooltip )
+            .on("mouseleave", hideTooltip )
+
+
+
+        // ---------------------------//
+        //       LEGEND              //
+        // ---------------------------//
+
+        legend_label_position_x = width - 70
+        legend_circle_position_x = width - 70
+        legend_circle_position_y = height - 30
+        legend_bubble_position_x = width - 110
+
+
+        // Legend: bubble sizes
+        var valuesToShow = [50, 1000, 5000]
+        var xCircle = legend_bubble_position_x
+        var xLabel = legend_label_position_x
+        bubble_svg
+            .selectAll("legend")
+            .data(valuesToShow)
+            .enter()
+            .append("circle")
+            .attr("cx", xCircle)
+            .attr("cy", function(d){ return legend_circle_position_y - z(d) } )
+            .attr("r", function(d){ return z(d) })
+            .style("fill", "none")
+            .attr("stroke", "black")
+
+        bubble_svg
+            .selectAll("legend")
+            .data(valuesToShow)
+            .enter()
+            .append("line")
+            .attr('x1', function(d){ return xCircle + z(d) } )
+            .attr('x2', xLabel)
+            .attr('y1', function(d){ return legend_circle_position_y - z(d) } )
+            .attr('y2', function(d){ return legend_circle_position_y - z(d) } )
+            .attr('stroke', 'black')
+            .style('stroke-dasharray', ('2,2'))
+
+        // Legend: labels for bubble sizes
+        bubble_svg
+            .selectAll("legend")
+            .data(valuesToShow)
+            .enter()
+            .append("text")
+            .attr('x', xLabel)
+            .attr('y', function(d){ return legend_circle_position_y - z(d) } )
+            .text( function(d){ return d } )
+            .attr("class", "bubble-legend")
+
+        // Legend title
+        bubble_svg.append("text")
+            .attr('x', xCircle)
+            .attr("y", legend_circle_position_y + 10)
+            .text("Number of pages")
+            .attr("text-anchor", "middle")
+            .attr("class", "bubble-legend")
+
+
+        // Legend dots
+        var size = 20
+
+        bubble_svg.selectAll("legend")
+         .data(lang_codes)
+         .enter()
+         .append("circle")
+           .attr("cx", legend_circle_position_x)
+           .attr("cy", function(d,i){ return 10 + i*(size+5)})
+           .attr("r", 7)
+           .style("fill", function(d){ return bubbleColor(d)})
+           .on("mouseover", highlight)
+           .on("mouseleave", noHighlight)
+
+        // Category labels for legend dots
+         bubble_svg.selectAll("legend")
+         .data(lang_codes)
+         .enter()
+         .append("text")
+           .attr("x", legend_label_position_x + size*.8)
+           .attr("y", function(d,i){ return i * (size + 5) + (size/2)})
+           .style("fill", function(d){ return bubbleColor(d)})
+           .text(function(d){ return languages[d]})
+           .attr("text-anchor", "left")
+           .attr("class", "bubble-legend")
+           .on("mouseover", highlight)
+           .on("mouseleave", noHighlight)
+
+    })
 }
 
-// When a legend category is not hovered anymore
-var noHighlight = function(d){
-    d3.selectAll(".bubbles").style("opacity", 1)
-}
-
-
-// ---------------------------//
-//       CIRCLES              //
-// ---------------------------//
-
-bubble_svg.append('g')
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("class", function(d) { return "bubbles " + d.language_code })
-    .attr("cx", function (d) {  return x(d3.timeParse("%d.%m.%Y")(d.publication_date)); })
-    .attr("cy", function (d) { return y(d.average_rating); })
-    .attr("r", function (d) { return z(d.num_pages); })
-    .style("fill", function (d) { return bubbleColor(d.language_code); })
-    .on("mouseover", showTooltip )
-    .on("mousemove", moveTooltip )
-    .on("mouseleave", hideTooltip )
-
-
-
-// ---------------------------//
-//       LEGEND              //
-// ---------------------------//
-
-legend_label_position_x = width - 70
-legend_circle_position_x = width - 70
-legend_circle_position_y = height - 30
-legend_bubble_position_x = width - 110
-
-
-// Legend: bubble sizes
-var valuesToShow = [50, 1000, 5000]
-var xCircle = legend_bubble_position_x
-var xLabel = legend_label_position_x
-bubble_svg
-    .selectAll("legend")
-    .data(valuesToShow)
-    .enter()
-    .append("circle")
-    .attr("cx", xCircle)
-    .attr("cy", function(d){ return legend_circle_position_y - z(d) } )
-    .attr("r", function(d){ return z(d) })
-    .style("fill", "none")
-    .attr("stroke", "black")
-
-bubble_svg
-    .selectAll("legend")
-    .data(valuesToShow)
-    .enter()
-    .append("line")
-    .attr('x1', function(d){ return xCircle + z(d) } )
-    .attr('x2', xLabel)
-    .attr('y1', function(d){ return legend_circle_position_y - z(d) } )
-    .attr('y2', function(d){ return legend_circle_position_y - z(d) } )
-    .attr('stroke', 'black')
-    .style('stroke-dasharray', ('2,2'))
-
-// Legend: labels for bubble sizes
-bubble_svg
-    .selectAll("legend")
-    .data(valuesToShow)
-    .enter()
-    .append("text")
-    .attr('x', xLabel)
-    .attr('y', function(d){ return legend_circle_position_y - z(d) } )
-    .text( function(d){ return d } )
-    .attr("class", "bubble-legend")
-
-// Legend title
-bubble_svg.append("text")
-    .attr('x', xCircle)
-    .attr("y", legend_circle_position_y + 10)
-    .text("Number of pages")
-    .attr("text-anchor", "middle")
-    .attr("class", "bubble-legend")
-
-
-// Legend dots
-var size = 20
-
-bubble_svg.selectAll("legend")
- .data(lang_codes)
- .enter()
- .append("circle")
-   .attr("cx", legend_circle_position_x)
-   .attr("cy", function(d,i){ return 10 + i*(size+5)})
-   .attr("r", 7)
-   .style("fill", function(d){ return bubbleColor(d)})
-   .on("mouseover", highlight)
-   .on("mouseleave", noHighlight)
-
-// Category labels for legend dots
- bubble_svg.selectAll("legend")
- .data(lang_codes)
- .enter()
- .append("text")
-   .attr("x", legend_label_position_x + size*.8)
-   .attr("y", function(d,i){ return i * (size + 5) + (size/2)})
-   .style("fill", function(d){ return bubbleColor(d)})
-   .text(function(d){ return languages[d]})
-   .attr("text-anchor", "left")
-   .attr("class", "bubble-legend")
-   .on("mouseover", highlight)
-   .on("mouseleave", noHighlight)
-
-
-}
